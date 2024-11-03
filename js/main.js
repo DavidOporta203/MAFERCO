ab356a8c04a92b6fa14b4340b2df42f16a8c7ecb
DavidOporta203 <davidoporta17@gmail.com>
2024-11-03 13:01:14 -0600

    Sidebar added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const menu = document.getElementById('menu');
+const sidebar = document.getElementById('sidebar');
+const main = document.getElementById('main');
+
+menu.addEventListener('click', ()=>{
+    sidebar.classList.toggle('menu-toggle')
+    menu.classList.toggle('menu-toggle');
+    main.classList.toggle('menu-toggle');
+})
+
 //Listener para realizar animación del apartado partner
 window.addEventListener('scroll', () => {
     const partnerItems = document.querySelectorAll('.partner-item');
