60fa0c347f20559cf97f202e9370048edf7ed2ae
xAndresCr <andresscastilloo81@gmail.com>
2024-11-29 22:06:07 -0600

    Form ready to make payments, input validations still missing and new form for in-store purchases

--- js/shoppingcart.js
+++
@@ -1,6 +0,0 @@
-
-
-    document.getElementById('btn-pagar').addEventListener('click', function() {
-        // Desplazar la página al formulario de pago
-        document.getElementById('formulario-pago').scrollIntoView({ behavior: 'smooth' });
-    });
